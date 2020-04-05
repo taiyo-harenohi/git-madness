a3589021bf92a275b3fd8f5e2bf7599d2b7e186f
taiyo <hare.no.hi.official@gmail.com>
2020-04-05 14:59:15 +0200

    Ádd Main Menu; Settings doesnt work

---
+++ Assets/Scripts/LevelChose.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class LevelChose : MonoBehaviour
+{
+    public void ChoseLevel()
+    {
+        switch (EventSystem.current.currentSelectedGameObject.name)
+        {
+            case "Level1":
+                SceneManager.LoadScene("maingame");
+                break;
+            /*case "Chapter 2":
+                SceneManager.LoadScene("story2");
+                break;
+            case "Chapter 3":
+                SceneManager.LoadScene("story3");
+                break;
+            case "Chapter 4":
+                SceneManager.LoadScene("story4");
+                break;
+            case "Chapter 5":
+                SceneManager.LoadScene("story5");
+                break;*/
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject levels;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("maingame");
+    }
+
+    public void Levels()
+    {
+        mainMenu.SetActive(false);
+        levels.SetActive(true);
+    }
+
+    public void Settings()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void Back()
+    {
+        mainMenu.SetActive(true);
+        levels.SetActive(false);
+        settingsMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
